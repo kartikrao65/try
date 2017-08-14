63a3c046866ba6bb90e38ec1d66b686fa897ebab
Kartheek Rao <30475093+kartikrao65@users.noreply.github.com>
2017-08-14 21:26:39 +0530

    Update file.js

--- file.js
+++ file.js
@@ -6,7 +6,8 @@ function moveRight () {
 	marginLeft=marginLeft+10;
 	img.style.marginLeft= marginLeft+'px';
 };
-  img.onclick = function() {
+  img.onclick = function() 
+  {
 	var interval= setInterval(moveRight,100);
 	
 };
