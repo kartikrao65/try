329729bf5c0104e2b5c2fd8c13a3285c92eb4198
Kartheek Rao <30475093+kartikrao65@users.noreply.github.com>
2017-08-14 20:33:46 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,10 @@
+var img = document.getElementById('kartik');
+var marginLeft=0;
+function moveRight () {
+	marginLeft=marginLeft+10;
+	img.style.marginLeft= marginLeft+'px';
+};
+  img.onclick = function() {
+	var interval= setInterval(moveRight,100);
+	
+};
